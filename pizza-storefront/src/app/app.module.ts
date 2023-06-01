006c5c1e573cb691591f356df0dcb813c8a5fa28
Caleb <5322420+calebnjw@users.noreply.github.com>
2023-06-01 11:46:28 +0800

    added routes

--- pizza-storefront/src/app/app.module.ts
+++ pizza-storefront/src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './components/main.component';
+import { MainComponent } from './components/main/main.component';
+import { OrdersComponent } from './components/orders/orders.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'orders/:email', component: OrdersComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent,
-  ],
+  declarations: [AppComponent, MainComponent, OrdersComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes, { useHash: true }),
   ],
 
-  providers: [ ],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
